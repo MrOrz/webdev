f09ee2ef23f8f0650c4536222d513edd21cdfb48
MrOrz <johnsonliang7@gmail.com>
2012-03-21 23:21:04 +0800

    use thin instead of webrick

--- serve.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-#: http://stackoverflow.com/questions/3108395/serve-current-directory-from-command-line
-
-require 'webrick'
-include WEBrick
-
-s = HTTPServer.new(:Port => 9090,  :DocumentRoot => Dir::pwd)
-trap("INT"){ s.shutdown }
-s.start

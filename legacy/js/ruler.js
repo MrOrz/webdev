dd2005da2b679c5e10ccc8365702eff60859a8a8
MrOrz <johnsonliang7@gmail.com>
2017-03-20 01:30:47 +0800

    Remove legacy

--- legacy/js/ruler.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  "use strict";
-  var $ruler = $('<div id="ruler"><span class="text"></span></div>').draggable().resizable().appendTo('body'),
-      $text = $ruler.find('.text');
-  $ruler.on('resize', function(e, ui){
-    $text.css('line-height', $ruler.innerHeight() + 'px');
-    $text.text($ruler.outerWidth() + ' x ' + $ruler.outerHeight());
-  });
-}());
--- legacy/sample/included.js
+++
@@ -1,1 +0,0 @@
-document.write("js 檔裡好溫暖喔 (羞)");
